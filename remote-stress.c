9b3c12813247927441cac38acb9213df4fa5ccda
Scott Pakin <pakin@lanl.gov>
2015-01-13 15:51:07 -0700

    Initial attempt to write a Makefile stress test
    The code doesn't completely work but represents a good enough start to
    commit it to the repository.

---
+++ remote-stress.c
@@ -0,0 +1,292 @@
+/****************************************
+ * Send commands to an execution server *
+ *                                      *
+ * By Scott Pakin <pakin@lanl.gov>      *
+ ****************************************/
+
+#include "makeint.h"
+#include "filedef.h"
+#include "job.h"
+#include "commands.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#ifndef UNIX_PATH_MAX
+# define UNIX_PATH_MAX 108
+#endif
+
+#define MAX_REQUEST_LEN 100   /* Maximum bytes in a fixed-length request to the server */
+#define MAX_RESPONSE_LEN 100   /* Maximum bytes in a response from the server */
+
+/* Define some global variables. */
+char *remote_description = "Stress Tester";  /* Identify ourself to GNU Make */
+static int have_server = 0;       /* 1=use stress-test server; 0=ordinary make */
+static char *socket_name = NULL;  /* Name of the Unix-domain socket for server communication */
+
+/* Given a string, escape double quotes and backslashes.  The caller
+   should free() the result.  We don't currently handle Unicode
+   characters, sorry. */
+static char *
+quote_for_json (const char *str)
+{
+  char *newstr = (char *) malloc(strlen(str)*2 + 3);  /* Assume all characters are quoted. */
+  const char *c;
+  char *nc = newstr;
+
+  for (c = str; *c; c++)
+    switch (*c) {
+      case '"':
+      case '\\':
+	*nc++ = '\\';
+	*nc++ = *c;
+	break;
+
+      case '\n':
+	*nc++ = '\\';
+	*nc++ = 'n';
+	break;
+	
+      case '\r':
+	*nc++ = '\\';
+	*nc++ = 'r';
+	break;
+	
+      case '\t':
+	*nc++ = '\\';
+	*nc++ = 't';
+	break;
+	
+      case '\b':
+	*nc++ = '\\';
+	*nc++ = 'b';
+	break;
+	
+      case '\f':
+	*nc++ = '\\';
+	*nc++ = 'f';
+	break;
+
+      default:	
+	*nc++ = *c;
+	break;
+    }
+  *nc = '\0';
+  return newstr;	
+}
+
+/* Send a request to the server and receive a response.  Return a
+   pointer to a static buffer containing the response or NULL on
+   error. */
+static const char *
+request_response (const char *request)
+{
+  int local_socket;         /* Socket to use to talk to the server */
+  struct sockaddr_un addr;  /* Address on which the stress-test server is listening */
+  static char response[MAX_RESPONSE_LEN + 1];   /* Response from the server */
+
+  fprintf(stderr, ">>> REQUEST: %s <<<\n", request);  // Temporary
+
+  /* Create a local socket. */
+  local_socket = socket(PF_LOCAL, SOCK_STREAM, 0);
+  if (local_socket == -1)
+    goto failure;
+
+  /* Establish a connection to the server. */
+  memset(&addr, 0, sizeof(struct sockaddr_un));
+  addr.sun_family = AF_LOCAL;
+  strncpy(addr.sun_path, socket_name, UNIX_PATH_MAX - 1);
+  if (connect(local_socket, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1)
+    goto failure;
+
+  /* Send the request. */
+  if (send(local_socket, request, strlen(request), 0) == -1)
+    goto failure;
+  if (shutdown(local_socket, SHUT_WR) == -1)
+    goto failure;
+
+  /* Prepare a response. */
+  memset(response, 0, MAX_RESPONSE_LEN + 1);
+  if (recv(local_socket, response, MAX_RESPONSE_LEN, MSG_WAITALL) < 0)
+    goto failure;
+
+  /* Return the message. */
+  (void) close(local_socket);
+  fprintf(stderr, ">>> RESPONSE: %s <<<\n", response);  // Temporary
+  return response;
+
+failure:
+  message(1, "Failed to communicate with the stress-testing server");
+  have_server = 0;
+  return NULL;
+}
+
+/* Determine if we were launched by the stress-tester front end. */
+void
+remote_setup (void)
+{
+  socket_name = getenv("STRESSMAKE_SOCKET");
+  if (socket_name == NULL) {
+    message(1, "Not performing Makefile stress-testing (not run from stress-make)");
+    return;
+  }
+  socket_name = strdup(socket_name);
+  have_server = 1;
+  message(1, "Performing Makefile stress-testing");
+}
+
+/* Perform any required cleanup actions. */
+void
+remote_cleanup (void)
+{
+}
+
+/* Return nonzero if the next job should be done remotely. */
+int
+start_remote_job_p (int first_p UNUSED)
+{
+  return have_server;
+}
+
+/* Start a remote job running the command in ARGV,
+   with environment from ENVP.  It gets standard input from STDIN_FD.  On
+   failure, return nonzero.  On success, return zero, and set *USED_STDIN
+   to nonzero if it will actually use STDIN_FD, zero if not, set *ID_PTR to
+   a unique identification, and set *IS_REMOTE to zero if the job is local,
+   nonzero if it is remote (meaning *ID_PTR is a process ID).  */
+int
+start_remote_job (char **argv, char **envp, int stdin_fd,
+                  int *is_remote, int *id_ptr,
+                  int *used_stdin)
+{
+  char *request = NULL;  /* Message to send */
+  char *rp;          /* Pointer into request */
+  char *fragment;    /* Fragment of a JSON message to send to the server */
+  char **cp;         /* Pointer into argv or envp */
+  ssize_t nBytes;    /* Number of bytes to send */
+  const char *response;   /* Response from server */
+
+  /* Do nothing if we were not run from the stress-test server. */
+  if (!have_server)
+    return -1;
+
+  /* Bound the size of the request. */
+  nBytes = 100;     /* Fixed contents, rounded up a bit */
+  for (cp = argv; *cp; cp++) {
+    fragment = quote_for_json(*cp);
+    nBytes += strlen(fragment) + 4;    /* String + two double quotes + comma + space */
+    free((void *)fragment);    
+  }
+  for (cp = envp; *cp; cp++) {
+    fragment = quote_for_json(*cp);
+    nBytes += strlen(fragment) + 4;    /* String + two double quotes + comma + space */
+    free((void *)fragment);    
+  }
+  request = (char *)malloc(nBytes);
+  if (request == NULL)
+    goto failure;
+
+  /* Construct a job-launch request. */
+  rp = request;
+  rp += sprintf(rp, "{\"Request\": \"spawn\", \"Args\": [");
+  for (cp = argv; *cp; cp++) {
+    fragment = quote_for_json(*cp);
+    rp += sprintf(rp, "\"%s\", ", fragment);
+    free((void *)fragment);
+  }
+  rp -= 2;  /* Drop the final comma and space. */
+  rp += sprintf(rp, "], \"Environ\": [");
+  for (cp = envp; *cp; cp++) {
+    fragment = quote_for_json(*cp);
+    rp += sprintf(rp, "\"%s\", ", fragment);
+    free((void *)fragment);
+  }
+  rp -= 2;  /* Drop the final comma and space. */
+  rp += sprintf(rp, "], \"Pid\": %ld}", (long int)getpid());
+
+  /* Send a job-launch request to the stress-test server. */
+  response = request_response(request);
+  if (response == NULL)
+    goto failure;
+  *is_remote = 1;
+  *id_ptr = atoi(response);
+  *used_stdin = 0;
+  return 0;
+
+ failure:
+  if (request != NULL)
+    free((void *)request);
+  return -1;
+}
+
+/* Get the status of a dead remote child.  Block waiting for one to die
+   if BLOCK is nonzero.  Set *EXIT_CODE_PTR to the exit status, *SIGNAL_PTR
+   to the termination signal or zero if it exited normally, and *COREDUMP_PTR
+   nonzero if it dumped core.  Return the ID of the child that died,
+   0 if we would have to block and !BLOCK, or < 0 if there were none.  */
+int
+remote_status (int *exit_code_ptr, int *signal_ptr,
+               int *coredump_ptr, int block)
+{
+  struct sockaddr_un addr;   /* Address on which the stress-test server is listening */
+  char request[MAX_REQUEST_LEN + 1];  /* Message to send or receive */
+  const char *response;      /* Response from server */
+  int pid;                   /* Process ID to return */
+
+  /* Do nothing if we were not run from the stress-test server. */
+  if (!have_server)
+    return -1;
+
+  /* Construct a status request. */
+  sprintf(request, "{\"Request\": \"status\", \"Block\": %s, \"Pid\": %ld}",
+	  block ? "true" : "false", (long int)getpid());
+
+  /* Send a status request to the stress-test server. */
+  response = request_response(request);
+  if (response == NULL)
+    return -1;
+  if (sscanf(response, "%d %d %d %d", &pid, exit_code_ptr, signal_ptr, coredump_ptr) != 4)
+    return -1;
+  return pid;
+}
+
+/* Block asynchronous notification of remote child death.
+   If this notification is done by raising the child termination
+   signal, do not block that signal.  */
+void
+block_remote_children (void)
+{
+}
+
+/* Restore asynchronous notification of remote child death.
+   If this is done by raising the child termination signal,
+   do not unblock that signal.  */
+void
+unblock_remote_children (void)
+{
+}
+
+/* Send signal SIG to child ID.  Return 0 if successful, -1 if not.  */
+int
+remote_kill (int id, UNUSED int sig)
+{
+  struct sockaddr_un addr;   /* Address on which the stress-test server is listening */
+  char request[MAX_REQUEST_LEN]; /* Message to send or receive */
+  const char *response;      /* Response from server */
+  int pid;          /* Process ID to return */
+
+  /* Do nothing if we were not run from the stress-test server. */
+  if (!have_server)
+    return -1;
+
+  /* Construct a status request. */
+  sprintf(request, "{\"Request\": \"kill\", \"Victim\": %d, \"Pid\": %ld}",
+	  id, (long int)getpid());
+
+  /* Send a status request to the stress-test server. */
+  response = request_response(request);
+  if (response == NULL)
+    return -1;
+  return atoi(response);
+}
---
+++ stress-make.go
@@ -0,0 +1,483 @@
+// Run a process-creation server and point our customized GNU Make at it.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path"
+	"strconv"
+	"sync/atomic"
+	"syscall"
+	"time"
+)
+
+// makeExecutable is the full path to our customized GNU Make.
+const makeExecutable = "/tmp/gnumake/bin/make"
+
+// Define a dequeue order.
+type DequeueOrder int
+
+// Define the order that we run enqueued processes.
+const (
+	FIFOOrder   = DequeueOrder(iota) // First in, first out
+	LIFOOrder                        // Last in, first out
+	RandomOrder                      // Random order
+)
+
+// deqOrder specifies the order in which we dequeue commands to run.
+var deqOrder DequeueOrder = LIFOOrder
+
+// maxLiveChildren specifies the maximum number of children we allow
+// to execute concurrently.
+var maxLiveChildren int64 = 1
+
+// currentLiveChildren specifies the number of children that are
+// currently executing.
+var currentLiveChildren int64 = 0
+
+// ChildCmd extends *exec.Cmd with a fake PID to return to GNU Make.
+type ChildCmd struct {
+	*exec.Cmd
+	FakePid uint64
+}
+
+// completedCommands reports children that have completed on a per-PID
+// basis (i.e., PID of the GNU Make process requesting the child).
+var completedCommands map[uint64]chan ChildCmd
+
+// fakePidToCmd maps a fabricated process ID to the associated command
+// structure.
+var fakePidToCmd map[uint64]*exec.Cmd
+
+// maxPid is the maximum process ID allowed by the OS.
+var maxPid uint64 = 32768
+
+// nextPid is the next available (fake) process ID we should return.
+var nextFakePid uint64 = 2
+
+// prng is a pseudorandom-number generator.
+var prng *rand.Rand
+
+// An empty represents zero-byte storage.
+type empty struct{}
+
+// createSocket chooses a random name for a Unix-domain socket and opens it.
+func createSocket() (string, *net.UnixListener) {
+	// Define the parameters for name creation.
+	const numChars = 6 // Number of random characters to include
+	const charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	const charSetLen = len(charSet)
+	baseName := path.Join(os.TempDir(), "stress-make-")
+
+	// Repeatedly choose names until socket creation is successful.
+	for {
+		// Randomly construct a socket name.
+		sockName := baseName
+		for i := 0; i < numChars; i++ {
+			sockName += string(charSet[prng.Intn(charSetLen)])
+		}
+
+		// Try creating a socket with that name.  Return if
+		// successful; try again if not.
+		addr, err := net.ResolveUnixAddr("unix", sockName)
+		if err != nil {
+			continue
+		}
+		listener, err := net.ListenUnix("unix", addr)
+		if err == nil {
+			return sockName, listener
+		}
+	}
+}
+
+// readProcUint64 reads a uint64 from Linux's proc filesystem.
+func readProcUint64(procFile string) (value uint64, err error) {
+	var fd *os.File
+	fd, err = os.Open(procFile)
+	if err != nil {
+		return
+	}
+	defer fd.Close()
+	sizeStr := make([]byte, 25)
+	nRead, err := fd.Read(sizeStr)
+	if err != nil {
+		return
+	}
+	value, err = strconv.ParseUint(string(sizeStr[:nRead]), 10, 64)
+	return
+}
+
+// A queueRequest is used internally to request updates to global data
+// structures.  MakePid and RespChan must always be specified.  If neither
+// Command nor Killpid are specified, this requests that a pending shell
+// command be run.
+type queueRequest struct {
+	MakePid  uint64      // Process ID (from a GNU Make process) that initiated the request
+	Command  *exec.Cmd   // Command to enqueue on the pending queue
+	KillPid  uint64      // (Fake) process ID to kill
+	RespChan chan uint64 // Response (success code or PID)
+}
+
+// qReqChan is the channel used to talk to the single instance of UpdateState.
+var qReqChan chan queueRequest
+
+// UpdateState safely updates global state in a concurrent context.  Much of
+// what the program actually does is encapsulated in the UpdateState routine.
+func UpdateState() {
+	allPendingCommands := make([]uint64, 0, maxLiveChildren) // All pending commands (by fake PID) across all Make processes
+	pidPendingCommands := make(map[uint64]map[uint64]empty)  // Subset of allPendingCommands local to a Make PID
+	for qReq := range qReqChan {
+		if qReq.RespChan == nil {
+			log.Fatal("Internal error processing a queue request")
+		}
+		mkPid := qReq.MakePid
+		switch {
+		case qReq.Command != nil:
+			// Allocate a new fake PID.
+			fPid := nextFakePid
+			fakePidToCmd[fPid] = qReq.Command
+			if nextFakePid == maxPid {
+				nextFakePid = 2
+			} else {
+				nextFakePid++
+			}
+
+			// Enqueue a new pending command on both the global
+			// list and the per-PID set.
+			allPendingCommands = append(allPendingCommands, fPid)
+			pidMap, ok := pidPendingCommands[mkPid]
+			if !ok {
+				// This is the first request from PID MakePid.
+				pidMap = make(map[uint64]empty, maxLiveChildren)
+				pidPendingCommands[mkPid] = pidMap
+			}
+			pidMap[fPid] = *&empty{}
+
+			// Return the fake PID.
+			qReq.RespChan <- fPid
+			close(qReq.RespChan)
+
+		case qReq.KillPid > 0:
+			// Kill the job if it's running.
+			fPid := qReq.KillPid
+			cmd, ok := fakePidToCmd[fPid]
+			if !ok {
+				// Error case (invalid fake PID)
+				qReq.RespChan <- 0
+				close(qReq.RespChan)
+				continue
+			}
+			if cmd.Process != nil {
+				// Job has launched.  Kill it.
+				if cmd.Process.Signal(os.Kill) != nil {
+					ok = false
+				}
+			}
+
+			// Whether or not the job has launched, elide the
+			// command from allPendingCommands, and delete it from
+			// pidPendingCommands.  Also delete the association
+			// between the fake PID and the command.
+			delete(fakePidToCmd, fPid)
+			for i := range allPendingCommands {
+				if allPendingCommands[i] == fPid {
+					allPendingCommands = append(allPendingCommands[:i], allPendingCommands[i+1:]...)
+					delete(pidPendingCommands[mkPid], fPid)
+					break
+				}
+			}
+			if ok {
+				qReq.RespChan <- 1
+			} else {
+				qReq.RespChan <- 0
+			}
+			close(qReq.RespChan)
+			continue
+
+		default:
+			// Ensure we have a command to run.
+			nPending := len(allPendingCommands)
+			if nPending == 0 {
+				// Something is seriously wrong.  Make thought
+				// it had pending commands, but it didn't.
+				qReq.RespChan <- 0
+				close(qReq.RespChan)
+				continue
+			}
+
+			// Run as many commands as we can.
+			for nPending > 0 && atomic.LoadInt64(&currentLiveChildren) < maxLiveChildren {
+				// Select a pending command to run.
+				var idx int
+				switch deqOrder {
+				case FIFOOrder:
+					idx = 0
+				case LIFOOrder:
+					idx = nPending - 1
+				case RandomOrder:
+					idx = prng.Intn(nPending)
+				default:
+					log.Fatal("Internal error processing the dequeue order")
+				}
+
+				// Elide the command from allPendingCommands,
+				// and delete it from pidPendingCommands.
+				fPid := allPendingCommands[idx]
+				cmd := fakePidToCmd[fPid]
+				allPendingCommands = append(allPendingCommands[:idx], allPendingCommands[idx+1:]...)
+				delete(pidPendingCommands[mkPid], fPid)
+				nPending--
+
+				// Create a completion queue if necessary.
+				if _, ok := completedCommands[mkPid]; !ok {
+					completedCommands[mkPid] = make(chan ChildCmd, maxLiveChildren)
+				}
+
+				// Run the command in the background.
+				atomic.AddInt64(&currentLiveChildren, 1)
+				go func() {
+					// Run the command then "kill" it to
+					// remove it from fakePidToCmd.
+					err := cmd.Run()
+					if err != nil {
+						log.Fatal(err)
+					}
+					atomic.AddInt64(&currentLiveChildren, -1)
+					qRespChan := make(chan uint64)
+					qReqChan <- queueRequest{MakePid: mkPid, KillPid: fPid, RespChan: qRespChan}
+					_ = <-qRespChan
+					completedCommands[mkPid] <- ChildCmd{Cmd: cmd, FakePid: fPid}
+				}()
+			}
+			qReq.RespChan <- 1
+			close(qReq.RespChan)
+		}
+	}
+}
+
+// A RemoteQuery represents a query received from a socket.
+type RemoteQuery struct {
+	Request string   // Either "spawn", "status", or "kill"
+	Args    []string // spawn: Command to run plus all arguments
+	Environ []string // spawn: Environment to use (list of key=value pairs)
+	Block   bool     // status: true=wait; false=return immediately
+	Victim  uint64   // kill: Victim process ID (really a fake PID)
+	Pid     uint64   // all: GNU Make process ID
+}
+
+// enqueueCommand prepares a user-specified command for execution and
+// enqueues it on the pendingCommands queue.  It returns a (fake) PID.
+func enqueueCommand(query *RemoteQuery, conn *net.UnixConn) {
+	if query.Args == nil || len(query.Args) == 0 {
+		log.Fatal("Received a spawn request with empty arguments")
+	}
+	if query.Environ == nil || len(query.Environ) == 0 {
+		log.Fatal("Received a spawn request with an empty environment")
+	}
+	if query.Pid == 0 {
+		log.Fatal("Received a spawn request with an unspecified PID")
+	}
+	cmd := exec.Command(query.Args[0], query.Args[1:]...)
+	cmd.Env = query.Environ
+	for _, keyval := range query.Environ {
+		if keyval[:4] == "PWD=" {
+			cmd.Dir = keyval[4:]
+			break
+		}
+	}
+	if cmd.Dir == "" {
+		log.Fatal("Spawn request did not include PWD in the environment")
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	qRespChan := make(chan uint64)
+	qReqChan <- queueRequest{MakePid: query.Pid, Command: cmd, RespChan: qRespChan}
+	fmt.Fprint(conn, <-qRespChan)
+}
+
+// awaitCommand polls or blocks for an arbitrary shell command to finish and
+// sends some status information on a given socket.  If no command is running,
+// choose one to run.
+func awaitCommand(query *RemoteQuery, conn *net.UnixConn) {
+	// Sanity-check the query.
+	if query.Pid == 0 {
+		log.Fatal("Received a status request with an unspecified PID")
+	}
+	mkPid := query.Pid
+
+	// Launch as many new commands as possible,
+	qRespChan := make(chan uint64)
+	qReqChan <- queueRequest{MakePid: mkPid, RespChan: qRespChan}
+	if <-qRespChan == 0 {
+		// Something went wrong -- probably that there are no new
+		// commands to run.  Notify the caller.
+		fmt.Fprint(conn, "-1 -1 -1 -1")
+		return
+	}
+
+	// If we were asked to block, then block until a running command
+	// finishes.  Otherwise, simply test if any of our running commands
+	// finished.
+	var cmd ChildCmd
+	if query.Block {
+		cmd = <-completedCommands[mkPid]
+	} else {
+		select {
+		case cmd = <-completedCommands[mkPid]:
+		}
+	}
+	if cmd.FakePid != 0 {
+		// A command finished -- send a reply with exit information.
+		status, ok := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		if !ok {
+			log.Fatal("Failed to retrieve the Unix wait status")
+		}
+		exitStatus := 0
+		if status.Exited() {
+			exitStatus = status.ExitStatus()
+		}
+		stopSignal := 0
+		if status.Signaled() {
+			stopSignal = int(status.StopSignal())
+		}
+		dumpedCore := 0
+		if status.CoreDump() {
+			dumpedCore = 1
+		}
+		fmt.Fprintf(conn, "%d %d %d %d",
+			cmd.FakePid, exitStatus, stopSignal, dumpedCore)
+		return
+	}
+
+	// No command finished.  Tell the caller to be patient.
+	fmt.Fprint(conn, "0 -1 -1 -1")
+}
+
+// killProcess sends a kill signal to a pending or running process.
+func killProcess(query *RemoteQuery, conn *net.UnixConn) {
+	// Sanity-check the query.
+	if query.Pid == 0 {
+		log.Fatal("Received a kill request with an unspecified PID")
+	}
+	mkPid := query.Pid
+	if query.Victim == 0 {
+		log.Fatal("Received a kill request with an unspecified victim PID")
+	}
+	fPid := query.Victim
+
+	// Kill the process.
+	qRespChan := make(chan uint64)
+	qReqChan <- queueRequest{MakePid: mkPid, KillPid: fPid, RespChan: qRespChan}
+	if <-qRespChan == 0 {
+		// Something went wrong -- probably that there the victim PID
+		// is invalid.
+		fmt.Fprint(conn, "-1")
+	} else {
+		// The process can now be considered dead.
+		fmt.Fprint(conn, "0")
+	}
+}
+
+// processQuery reads a single query from a socket and, if
+// appropriate, sends back a response.
+func processQuery(conn *net.UnixConn) {
+	// Read a query from the given socket.
+	defer conn.Close()
+	jsonDec := json.NewDecoder(conn)
+	var query RemoteQuery
+	jsonDec.Decode(&query)
+
+	// Process the query.
+	switch query.Request {
+	case "spawn":
+		enqueueCommand(&query, conn)
+	case "status":
+		awaitCommand(&query, conn)
+	case "kill":
+		killProcess(&query, conn)
+	default:
+		log.Fatalf("Received unrecognized request %q", query.Request)
+	}
+}
+
+// serverLoop repeatedly accepts queries from a socket and processes them.
+func serverLoop(lst *net.UnixListener) {
+	for {
+		conn, err := lst.AcceptUnix()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go processQuery(conn)
+	}
+}
+
+// spawnMake accepts a socket name and a list of arguments (excluding
+// the command name) and spawns our customized version of GNU Make.
+func spawnMake(sockName string, argList []string) {
+	// Point all calls to make to our customized version.
+	os.Setenv("PATH", path.Dir(makeExecutable)+":"+os.Getenv("PATH"))
+	os.Setenv("MAKE", makeExecutable)
+	os.Setenv("STRESSMAKE_SOCKET", sockName)
+
+	// Launch our customized GNU Make and wait for it to complete.
+	cmd := exec.Command(makeExecutable, argList...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// Prepare the logger for issuing warnings and errors.
+	log.SetFlags(0)
+	log.SetPrefix(os.Args[0] + ": ")
+
+	// Seed a pseudorandom-number generator.
+	seed := int64(os.Getpid()) * int64(os.Getppid()) * int64(time.Now().UnixNano())
+	prng = rand.New(rand.NewSource(seed))
+
+	// Determine the maximum process ID we're allowed to use.
+	var err error
+	maxPid, err = readProcUint64("/proc/sys/kernel/pid_max")
+	if err != nil {
+		maxPid = 32768
+	}
+	fakePidToCmd = make(map[uint64]*exec.Cmd)
+
+	// Launch an internal server for serializing accesses to global data
+	// structures.
+	completedCommands = make(map[uint64]chan ChildCmd)
+	qReqChan = make(chan queueRequest, maxLiveChildren)
+	go UpdateState()
+
+	// Create a socket and accept commands from it in the background.
+	sockName, listener := createSocket()
+	defer os.Remove(sockName)
+	go func() {
+		// If we receive an exit signal, remove the socket and
+		// resend the signal.
+		sigChan := make(chan os.Signal, 5)
+		signal.Notify(sigChan, os.Interrupt, os.Kill) // Can't really catch Kill.
+		sig := <-sigChan
+		signal.Stop(sigChan)
+		os.Remove(sockName)
+		proc, err := os.FindProcess(os.Getpid())
+		if err != nil {
+			log.Fatal(err)
+		}
+		proc.Signal(sig)
+	}()
+	go serverLoop(listener)
+
+	// Run our customized GNU Make as a child process.
+	spawnMake(sockName, os.Args[1:])
+}
